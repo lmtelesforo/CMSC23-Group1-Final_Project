3a61d1102905ed7435e5ab1e2957a47084d77f81
Laira Telesforo <lmtelesforo@up.edu.ph>
2024-05-18 13:35:29 +0800

    updated firebase_auth_api

--- cmsc23_project/lib/api/firebase_auth_api.dart
+++ cmsc23_project/lib/api/firebase_auth_api.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthAPI {
+  static final FirebaseAuth auth = FirebaseAuth.instance;
 
   User? getUser() {
     return auth.currentUser;
