4407dd3f7abc72ebc821a1e4aa5a781cd3fd9863
Laira Telesforo <lmtelesforo@up.edu.ph>
2024-05-18 13:05:15 +0800

    feat: installed firebase

--- cmsc23_project/lib/main.dart
+++ cmsc23_project/lib/main.dart
@@ -12,8 +12,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'admin_view/view_all_donations.dart';
 import 'providers/textfield_providers.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
