d03139c4ad23204270e2c86788797a17fa5e4dd3
zachglindro <zmglindro2@up.edu.ph>
2024-05-18 20:53:50 +0800

    refactor: modularize orgdrawer code

--- cmsc23_project/lib/org-view/drawer.dart
+++ cmsc23_project/lib/org-view/drawer.dart
@@ -6,48 +6,77 @@ class OrgDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: ListView(
-      children: [
-        ListTile(
-          title: const Text('Home'),
-          onTap: () {
-            if (ModalRoute.of(context)!.settings.name == '/orgHomepage') {
-              Navigator.pop(context);
-            } else {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/orgHomepage');
-            }
-          },
-        ),
-        ListTile(
-          title: const Text('Add a Drive'),
-          onTap: () {
-            if (ModalRoute.of(context)!.settings.name == '/add-a-drive') {
-              Navigator.pop(context);
-            } else {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/add-a-drive');
-            }
-          },
-        ),
-        ListTile(
-          title: const Text('Profile'),
-          onTap: () {
-            if (ModalRoute.of(context)!.settings.name == '/orgProfile') {
-              Navigator.pop(context);
-            } else {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/orgProfile');
-            }
-          },
-        ),
-        ListTile(
-          title: const Text('Log Out'),
-          onTap: () {
-            Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
-          },
-        ),
-      ],
-    ));
+      child: ListView(
+        children: [
+          _HomeTile(),
+          _AddDriveTile(),
+          _ProfileTile(),
+          _LogoutTile(),
+        ],
+      ),
+    );
+  }
+}
+
+class _LogoutTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('Log Out'),
+      onTap: () {
+        Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
+      },
+    );
+  }
+}
+
+class _ProfileTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('Profile'),
+      onTap: () {
+        if (ModalRoute.of(context)!.settings.name == '/orgProfile') {
+          Navigator.pop(context);
+        } else {
+          Navigator.pop(context);
+          Navigator.pushNamed(context, '/orgProfile');
+        }
+      },
+    );
+  }
+}
+
+class _AddDriveTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('Add a Drive'),
+      onTap: () {
+        if (ModalRoute.of(context)!.settings.name == '/add-a-drive') {
+          Navigator.pop(context);
+        } else {
+          Navigator.pop(context);
+          Navigator.pushNamed(context, '/add-a-drive');
+        }
+      },
+    );
+  }
+}
+
+class _HomeTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('Home'),
+      onTap: () {
+        if (ModalRoute.of(context)!.settings.name == '/orgHomepage') {
+          Navigator.pop(context);
+        } else {
+          Navigator.pop(context);
+          Navigator.pushNamed(context, '/orgHomepage');
+        }
+      },
+    );
   }
 }
